5f32bd17a4f569cdad59cb83a284fe98aa3380a2
CHUKONG-INC\zhenqiang.yan <yzqlwt@gmail.com>
2022-08-22 17:40:56 +0800

    feat: 修改为post

--- routes/xiaoai.js
+++ routes/xiaoai.js
@@ -1,8 +1,24 @@
 var express = require('express');
+const {json} = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+// router.get('/', function (req, res, next) {
+//     const content = {
+//         "version": "1.0",
+//         "response": {
+//             "open_mic": false,
+//             "to_speak":{
+//                 "type": 0,
+//                 "text": "好嘞，您稍等"
+//             }
+//         },
+//         "is_session_end": true
+//     }
+//     res.json(content);
+// });
+
+router.post('/', function (req, res, next) {
     const content = {
         "version": "1.0",
         "response": {
@@ -14,7 +30,7 @@ router.get('/', function (req, res, next) {
         },
         "is_session_end": true
     }
-    res.send(content);
+    res.json(content);
 });
 
 module.exports = router;
