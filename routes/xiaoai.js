54aaa6466b7aafc120dde11de66b75e34ce01d35
yzqlwt <yzqlwt@gmail.com>
2022-08-28 12:32:03 +0800

    wakeup

--- routes/xiaoai.js
+++ routes/xiaoai.js
@@ -1,5 +1,6 @@
 var express = require('express');
-const {json} = require("express");
+const { Client } = require('ssh2');
+const { json } = require("express");
 var router = express.Router();
 
 /* GET users listing. */
@@ -18,12 +19,35 @@ var router = express.Router();
 //     res.json(content);
 // });
 
+
+
 router.post('/', function (req, res, next) {
+    const conn = new Client();
+    conn.on('ready', () => {
+        console.log('Client :: ready');
+        conn.exec('node wakeup/wakeup.js', (err, stream) => {
+            if (err) throw err;
+            stream.on('close', (code, signal) => {
+                console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+                conn.end();
+            }).on('data', (data) => {
+                console.log('STDOUT: ' + data);
+            }).stderr.on('data', (data) => {
+                console.log('STDERR: ' + data);
+            });
+        });
+    }).connect({
+        host: 'www.yzqlwt.com',
+        port: 10004,
+        username: 'yzqlwt',
+        password: 'xuanli0912_'
+    });
     const content = {
         "version": "1.0",
         "response": {
             "open_mic": false,
-            "to_speak":{
+            "not_understand": false,
+            "to_speak": {
                 "type": 0,
                 "text": "好嘞，您稍等"
             }
